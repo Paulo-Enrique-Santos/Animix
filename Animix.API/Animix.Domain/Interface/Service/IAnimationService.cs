1751c7fb34150263a4f2382f92bcdc88dff734a8
Paulo Filho <paulo.filho@genial.com.vc>
2023-01-12 14:11:06 -0300

    feat: back - create animation interface service

--- Animix.API/Animix.Domain/Interface/Service/IAnimationService.cs
+++ Animix.API/Animix.Domain/Interface/Service/IAnimationService.cs
@@ -4,5 +4,10 @@ namespace Animix.Domain.Interface.Service
 {
     public interface IAnimationService
     {
+        Task<Animation> CreateAnimationAsync(Animation animation);
+        Task<List<Animation>> GetAllAnimationsAsync();
+        Task<Animation> GetAnimationByIdAsync(int idAnimation);
+        Task<Animation> UpdateAnimationAsync(int idAnimation);
+        Task<string> DeleteAnimationAsync(int idAnimation);
     }
 }
